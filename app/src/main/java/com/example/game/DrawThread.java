2f4146c080e074f088c15fd4ea3e6ab74b9999d9
Lidne <play.gt.play@yandex.ru>
2022-01-11 16:13:12 +0300

    made creation of player in main thread

---
+++ app/src/main/java/com/example/game/DrawThread.java
@@ -0,0 +1,76 @@
+package com.example.game;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.os.CountDownTimer;
+import android.view.SurfaceHolder;
+
+public class DrawThread extends Thread {
+
+    private SurfaceHolder surfaceHolder;
+
+    private volatile boolean running = true; //флаг для остановки потока
+    private Paint backgroundPaint = new Paint();
+    private final int timerInterval = 100;
+    private GameView view;
+    private Player player;
+
+    {
+        backgroundPaint.setColor(Color.BLACK);
+        backgroundPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public DrawThread(Context context, SurfaceHolder surfaceHolder, GameView view) {
+        this.surfaceHolder = surfaceHolder;
+        this.view = view;
+        this.player = view.getPlayer();
+
+        // таймер для отрисовки нового кадра анимации
+        Timer t = new Timer();
+        t.start();
+    }
+
+    public void requestStop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            Canvas canvas = surfaceHolder.lockCanvas();
+            if (canvas != null) {
+                try {
+                    synchronized (view.getHolder()) {
+                        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                        player.draw(canvas);
+                        update();
+                    }
+                } finally {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }
+    }
+
+    class Timer extends CountDownTimer {
+        public Timer() {
+            super(Integer.MAX_VALUE, timerInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            update();
+        }
+
+        @Override
+        public void onFinish() {
+        }
+    }
+
+    protected void update() {
+        player.update(timerInterval, MainActivity.strength, MainActivity.angle);
+    }
+}
--- app/src/main/java/com/example/game/GameView.java
+++ app/src/main/java/com/example/game/GameView.java
@@ -18,16 +18,24 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private DrawThread drawThread;
     private final int timerInterval = 30;
+    private Player player;
 
     public GameView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.fire_knight); // загружаем картинку с фреймами
+
+        int w = bitmap.getWidth()/28;
+        int h = bitmap.getHeight()/13;
+
+        player = new Player(100, 200, 200, 0, w, h, bitmap); // создаём героя
 
         getHolder().addCallback(this);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        drawThread = new DrawThread(getContext(), getHolder());
+        drawThread = new DrawThread(getContext(), getHolder(), this);
         drawThread.start();
     }
 
@@ -50,71 +58,11 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
-    public class DrawThread extends Thread {
+    public Player getPlayer() {
+        return player;
+    }
 
-        private SurfaceHolder surfaceHolder;
-        private Player player;
-
-        private volatile boolean running = true; //флаг для остановки потока
-        private Paint backgroundPaint = new Paint();
-        private final int timerInterval = 100;
-
-        {
-            backgroundPaint.setColor(Color.BLACK);
-            backgroundPaint.setStyle(Paint.Style.FILL);
-        }
-
-        public DrawThread(Context context, SurfaceHolder surfaceHolder) {
-            Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.fire_knight); // загружаем картинку с фреймами
-
-            int w = bitmap.getWidth()/28;
-            int h = bitmap.getHeight()/13;
-            player = new Player(100, 200, 200, 0, w, h, bitmap); // создаём героя
-
-            this.surfaceHolder = surfaceHolder;
-
-            // таймер для отрисовки нового кадра анимации
-            Timer t = new Timer();
-            t.start();
-        }
-
-        public void requestStop() {
-            running = false;
-        }
-
-        @Override
-        public void run() {
-            while (running) {
-                Canvas canvas = surfaceHolder.lockCanvas();
-                if (canvas != null) {
-                    try {
-                        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
-                        player.draw(canvas);
-                        update();
-                    } finally {
-                        surfaceHolder.unlockCanvasAndPost(canvas);
-                    }
-                }
-            }
-        }
-
-        class Timer extends CountDownTimer {
-            public Timer() {
-                super(Integer.MAX_VALUE, timerInterval);
-            }
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                update();
-            }
-
-            @Override
-            public void onFinish() {
-            }
-        }
-
-        protected void update() {
-            player.update(timerInterval, MainActivity.strength, MainActivity.angle);
-        }
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 }
