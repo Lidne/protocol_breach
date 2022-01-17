3fc1fdbc218bf33dd15a78614e128d182d25db69
Lidne <play.gt.play@yandex.ru>
2022-01-17 13:49:52 +0300

    fixed some bugs

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 public class MainActivity extends Activity {
     @Override
@@ -14,12 +15,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.buttonStart);
-        button.setOnClickListener (new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent switcher = new Intent(MainActivity.this,Game.class);
+            public void onClick(View v) {
+                Intent switcher = new Intent(MainActivity.this, Game.class);
                 MainActivity.this.startActivity(switcher);
-            }});
-    }}
+            }
+        });
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+    }
+}
 
 
